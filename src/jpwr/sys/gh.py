7f9127f4cd0f783c1ac268d7089b104a4c57f0cf
Stepan Nassyr <s.nassyr@fz-juelich.de>
2024-07-11 10:17:25 +0200

    Improve GH power method

--- src/jpwr/sys/gh.py
+++ src/jpwr/sys/gh.py
@@ -35,22 +35,34 @@ def power_loop(queue, event, interval):
 
 
 class power(object):
+    hwmon_base = "/sys/class/hwmon/"
     def init(self, power_value_dict : dict[str,list[float]]):
-        power_value_dict.update({
-            'gh:Module' : [],
-            'gh:Grace' : [],
-            'gh:CPU' : [],
-            'gh:SysIO' : [],
-        })
+        # sort by hwmon number
+        self.hwmons = sorted(os.listdir(self.hwmon_base),key=lambda x: int(x[5:]))
+        for hwmon in self.hwmons:
+            hwmon_path = os.path.join(self.hwmon_base, hwmon)
+            oem_path = os.path.join(hwmon_path, "device/power1_oem_info")
+            if not os.path.exists(oem_path):
+                continue
+            with open(oem_path, 'r') as oem_fd:
+                name = oem_fd.read()
+                name = name.strip()
+
+            power_value_dict[f"gh:{name}"] = []
     def measure(self, power_value_dict : dict[str,list[float]]):
-        with open("/sys/class/hwmon/hwmon1/device/power1_average") as f:
-            power_value_dict["gh:Module"].append(1e-6*float(f.read()))
-        with open("/sys/class/hwmon/hwmon2/device/power1_average") as f:
-            power_value_dict["gh:Grace"].append(1e-6*float(f.read()))
-        with open("/sys/class/hwmon/hwmon3/device/power1_average") as f:
-            power_value_dict["gh:CPU"].append(1e-6*float(f.read()))
-        with open("/sys/class/hwmon/hwmon4/device/power1_average") as f:
-            power_value_dict["gh:SysIO"].append(1e-6*float(f.read()))
+        for hwmon in self.hwmons:
+            hwmon_path = os.path.join(self.hwmon_base, hwmon)
+            oem_path = os.path.join(hwmon_path, "device/power1_oem_info")
+            if not os.path.exists(oem_path):
+                continue
+            with open(oem_path, 'r') as oem_fd:
+                name = oem_fd.read()
+                name = name.strip()
+            power_path = os.path.join(hwmon_path, "device/power1_average")
+            with open(power_path) as power_fd:
+                value = 1e-6*float(power_fd.read())
+
+            power_value_dict[f"gh:{name}"].append(value)
 
     def finalize(self, power_value_dict : dict[str,list[float]]):
         return {}
